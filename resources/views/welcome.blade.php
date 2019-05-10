72f83488859707180dd0600373f0926757b8f823
Bangrejas <aviefrejas@gmail.com>
2019-05-10 13:54:50 +0700

    kommit2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Yanuar Budi
+                    Avief Reja Satria
                 </div>
 
                 <div class="links">
@@ -92,6 +92,7 @@
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="/buku">Perpustakaan</a>
                 </div>
             </div>
         </div>
